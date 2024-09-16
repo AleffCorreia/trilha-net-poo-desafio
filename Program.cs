8406efd7e44331bdb817407e6eb2a6f8a2aee33f
Aleff Correia <aleffcorreia@outlook.com>
2024-09-15 23:48:43 -0300

    Atualização da versão do dotnet, inclusão das variáveis, metodos e teste

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456789", modelo: "Modelo Pro", imei: "1111111111",memoria: 265);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "123456789", modelo: "Modelo Pro 14", imei: "2222222222",memoria: 265);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
